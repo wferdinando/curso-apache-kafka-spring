6089d885b4616307d7238c3469d11a388b6728fc
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2023-06-19 21:51:15 -0300

    Kafka Admin and KafkaTemplate - producer created

---
+++ str-producer/src/main/java/com/wferdinando/strproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,35 @@
+package com.wferdinando.strproducer.config;
+
+import java.util.HashMap;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    public final KafkaProperties properties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("str-topic")
+                        .partitions(2)
+                        .replicas(1)
+                        .build());
+    }
+}
