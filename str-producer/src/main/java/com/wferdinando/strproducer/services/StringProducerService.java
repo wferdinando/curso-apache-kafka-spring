ecb7dfff75280a8bb5c4d48171dcdd48c15a8a1c
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2023-06-22 21:03:10 -0300

    str-consumer created

--- str-producer/src/main/java/com/wferdinando/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/wferdinando/strproducer/services/StringProducerService.java
@@ -14,15 +14,17 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
-                success -> {
-                    if (success != null) {
-                        log.info("Send message with success {}", message);
-                        log.info("Partition - {}, Offset - {} ", success.getRecordMetadata().partition(),
-                                success.getRecordMetadata().offset());
-                    }
-                },
-                error -> log.error("Error send message."));
+        log.info("Send message {}", message);
+        kafkaTemplate.send("str-topic", message);
+        // .addCallback(
+        //         success -> {
+        //             if (success != null) {
+        //                 log.info("Send message with success {}", message);
+        //                 log.info("Partition - {}, Offset - {} ", success.getRecordMetadata().partition(),
+        //                         success.getRecordMetadata().offset());
+        //             }
+        //         },
+        //         error -> log.error("Error send message."));
     }
 
 }
