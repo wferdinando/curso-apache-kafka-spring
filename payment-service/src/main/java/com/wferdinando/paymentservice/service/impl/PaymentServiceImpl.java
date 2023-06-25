273855454074a28ab075c9e6a947d2de8d9595fe
Willyan Ferdinando <willyantferdinando@gmail.com>
2023-06-25 20:51:56 -0300

    add producer config

--- payment-service/src/main/java/com/wferdinando/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/wferdinando/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,19 +1,33 @@
 package com.wferdinando.paymentservice.service.impl;
 
+import java.io.Serializable;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.wferdinando.paymentservice.model.Payment;
 import com.wferdinando.paymentservice.service.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-       log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento! {}", payment); 
+       log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento! {}", payment);
+       Thread.sleep(1000);
+       
+       log.info("Enviando pagamento . . .");
+       kafkaTemplate.send("payment-topic", payment); 
     }
     
 }
